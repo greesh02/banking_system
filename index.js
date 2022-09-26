803b0a44d8e67a7c8777c1ab0c640bfbf1482757
R S Greeshwar <greeshwargree@gmail.com>
2022-09-27 00:32:15 +0530

    added start script

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
+const path = require('path')
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname+"public"));
+app.use(express.static(path.join(__dirname,"public")));
 
 // mongoose.connect("mongodb://localhost:27017/bankDB");
 mongoose.connect("mongodb+srv://greesh:greeshwar@cluster0.fhm9b.mongodb.net/bankDB?retryWrites=true&w=majority");
